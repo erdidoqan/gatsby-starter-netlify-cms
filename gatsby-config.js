5aaadeb64e5f63a41c02236486277bc37f2446cb
Erdi <erdi.doqan@gmail.com>
2021-05-28 03:07:27 +0300

    yeni

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Digitexa Cloud - Topluluk',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Teknik öğreticiler, etkinlikler — Bu, geliştiricilerin destek bulabileceği veya ödünç verebileceği ve topluluğa katkıda bulunmanın yeni yollarını keşfedebileceği kapsayıcı bir yerdir.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
